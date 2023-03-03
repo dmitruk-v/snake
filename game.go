40be9bab1d69753c9aa14a004a7a2672e7de1a7d
Dmitruk Valeriy <valera.dmitruk@gmail.com>
2023-03-03 20:08:09 +0200

    updated graphics

--- game.go
+++ game.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"os/exec"
+	"sync"
 	"time"
 
 	"github.com/eiannone/keyboard"
@@ -26,87 +27,153 @@ const (
 	Right
 )
 
-type Game struct {
-	width      int
-	height     int
-	isGameOver bool
-	snake      Node
-	tail       []Node
-	score      int
-	fruits     [][]int
-	exitCh     chan struct{}
+type Difficulty int
+
+const (
+	Easy       Difficulty = 5
+	Medium     Difficulty = 6
+	Hard       Difficulty = 8
+	Impossible Difficulty = 12
+)
+
+var DifficultiesMap = map[Difficulty]string{
+	Easy:       "EASY",
+	Medium:     "MEDIUM",
+	Hard:       "HARD",
+	Impossible: "IMPOSSIBLE",
 }
 
-func NewGame(width, height int) *Game {
+type Result struct {
+	success bool
+	spent   time.Duration
+}
+
+type Game struct {
+	gameRand    *rand.Rand
+	width       int
+	height      int
+	isGameOver  bool
+	isCompleted bool
+	snake       Node
+	tail        []Node
+	score       int
+	topScore    int
+	diff        Difficulty
+	fruits      [][]int
+	exitCh      chan struct{}
+	keyCh       chan rune
+	wg          sync.WaitGroup
+}
+
+func NewGame(width, height int, topScore int, diff Difficulty) *Game {
 	fruits := make([][]int, width)
 	for i := 0; i < width; i++ {
 		fruits[i] = make([]int, height)
 	}
 	return &Game{
-		width:  width,
-		height: height,
-		fruits: fruits,
-		exitCh: make(chan struct{}),
+		gameRand: rand.New(rand.NewSource(time.Now().Unix())),
+		width:    width,
+		height:   height,
+		topScore: topScore,
+		diff:     diff,
+		fruits:   fruits,
+		exitCh:   make(chan struct{}),
+		keyCh:    make(chan rune, 1),
 	}
 }
 
-func (g *Game) Init() {
+func (g *Game) init() {
 	// Initiate
 	g.placeFruit()
 	g.snake = Node{X: g.width / 2, Y: g.height / 2, Dir: Right}
-	g.tail = []Node{
-		// {X: g.snake.X - 1, Y: g.snake.Y, Dir: g.snake.Dir},
-	}
+	// Hide cursor
+	fmt.Print("\x1b[?25l")
 }
 
-func (g *Game) Run() {
+func (g *Game) Run() Result {
 	// Init and run
-	g.Init()
+	g.init()
 
-	// Keyboard init
-	keysEvents, err := keyboard.GetKeys(10)
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		_ = keyboard.Close()
-	}()
-
+	g.wg.Add(1)
 	go func() {
-		for event := range keysEvents {
-			switch event.Rune {
-			case 'w':
-				if g.snake.Dir != Down {
-					g.snake.Dir = Up
-				}
-			case 's':
-				if g.snake.Dir != Up {
-					g.snake.Dir = Down
-				}
-			case 'a':
-				if g.snake.Dir != Right {
-					g.snake.Dir = Left
-				}
-			case 'd':
-				if g.snake.Dir != Left {
-					g.snake.Dir = Right
-				}
-			case 0:
-				close(g.exitCh)
+		keyEventCh, err := keyboard.GetKeys(0)
+		if err != nil {
+			panic(err)
+		}
+		defer func() {
+			_ = keyboard.Close()
+			g.wg.Done()
+		}()
+		for {
+			select {
+			case key := <-keyEventCh:
+				g.keyCh <- key.Rune
+			case <-g.exitCh:
+				close(g.keyCh)
 				return
 			}
 		}
 	}()
 
 	// Game loop
-	for !g.isGameOver {
-		g.clearTerm("clear")
+	begin := time.Now()
+	for {
+		// Signal to gorutines to exit
+		if g.isGameOver || g.isCompleted {
+			close(g.exitCh)
+			g.wg.Wait()
+		}
+		// Game over case
+		if g.isGameOver {
+			return Result{
+				success: false,
+				spent:   time.Since(begin),
+			}
+		}
+		// Success game case
+		if g.isCompleted {
+			return Result{
+				success: true,
+				spent:   time.Since(begin),
+			}
+		}
+		// g.clearTerm("clear")
+		g.gameInput()
 		g.gameLogic()
 		g.gameDraw()
-		fmt.Printf("SCORE: %v\n", g.score)
-		time.Sleep(time.Second / 5)
+
+		time.Sleep(time.Second / time.Duration(g.diff))
 	}
-	fmt.Println("GAME OVER!")
+}
+
+func (g *Game) gameInput() {
+	select {
+	case key := <-g.keyCh:
+		// React to keypress
+		switch {
+		case key == 'w':
+			if g.snake.Dir != Down {
+				g.snake.Dir = Up
+			}
+		case key == 's':
+			if g.snake.Dir != Up {
+				g.snake.Dir = Down
+			}
+		case key == 'a':
+			if g.snake.Dir != Right {
+				g.snake.Dir = Left
+			}
+		case key == 'd':
+			if g.snake.Dir != Left {
+				g.snake.Dir = Right
+			}
+		case key == 0:
+			g.gameOver()
+			return
+		}
+	default:
+		// skip
+	}
 }
 
 func (g *Game) gameLogic() {
@@ -114,6 +181,10 @@ func (g *Game) gameLogic() {
 	if g.fruits[g.snake.X][g.snake.Y] != 0 {
 		g.score++
 		g.fruits[g.snake.X][g.snake.Y] = 0
+		if g.score == g.topScore {
+			g.gameCompleted()
+			return
+		}
 		g.placeFruit()
 		// Add tail
 		last := g.snake
@@ -154,20 +225,13 @@ func (g *Game) gameLogic() {
 		g.snake.X++
 	}
 	// Check game over
-	select {
-	case <-g.exitCh:
-		g.isGameOver = true
-	default:
-		if g.doesWallsCollision() {
-			g.isGameOver = true
-		}
-		if g.doesSelfEat() {
-			g.isGameOver = true
-		}
+	if g.doesWallsCollision() || g.doesSelfEat() {
+		g.gameOver()
 	}
 }
 
 func (g *Game) gameDraw() {
+	fmt.Print("\x1b[1J\x1b[0;0H")
 	for y := 0; y < g.height; y++ {
 		for x := 0; x < g.width; x++ {
 			// Print top and bottom walls
@@ -202,15 +266,31 @@ func (g *Game) gameDraw() {
 			}
 			// Print fruit
 			if g.fruits[x][y] == 1 {
-				fmt.Print("F")
+				fmt.Print("\x1b[38;5;46mF\x1b[0m")
 				continue
 			}
 			// Print empty space
 			if !isTail {
-				fmt.Print(" ")
+				fmt.Print("\x1b[38;5;241m.\x1b[0m")
 			}
 		}
 	}
+	fmt.Printf("\x1b[38;5;226mSCORE: %v/%v\x1b[0m, DIFFICULTY: \x1b[38;5;207m%v\x1b[0m\n", g.score, g.topScore, DifficultiesMap[g.diff])
+	if g.isGameOver {
+		fmt.Println("\x1b[38;5;198mGAME OVER!\x1b[0m")
+	} else if g.isCompleted {
+		fmt.Println("\x1b[38;5;51mCOMPLETED!\x1b[0m")
+	}
+}
+
+func (g *Game) gameOver() {
+	g.isGameOver = true
+	fmt.Print("\x1b[?25h")
+}
+
+func (g *Game) gameCompleted() {
+	g.isCompleted = true
+	fmt.Print("\x1b[?25h")
 }
 
 func (g *Game) clearTerm(cmdName string) {
@@ -222,8 +302,8 @@ func (g *Game) clearTerm(cmdName string) {
 }
 
 func (g *Game) placeFruit() {
-	x := 1 + rand.Intn(g.width-2)
-	y := 1 + rand.Intn(g.height-2)
+	x := 1 + g.gameRand.Intn(g.width-2)
+	y := 1 + g.gameRand.Intn(g.height-2)
 	g.fruits[x][y] = 1
 }
 
--- main.go
+++ main.go
@@ -1,6 +1,55 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
 func main() {
-	game := NewGame(20, 10)
-	game.Run()
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+const delayBetweenGames = 3
+
+func run() error {
+	game := NewGame(30, 13, 5, Easy)
+	result := game.Run()
+	if result.success {
+		fmt.Println("EASY completed in", result.spent.Truncate(time.Millisecond))
+		fmt.Printf("MEDIUM in %v seconds...\n", delayBetweenGames)
+		time.Sleep(time.Second * delayBetweenGames)
+	} else {
+		return nil
+	}
+
+	game = NewGame(35, 15, 10, Medium)
+	result = game.Run()
+	if result.success {
+		fmt.Println("MEDIUM completed in", result.spent.Truncate(time.Millisecond))
+		fmt.Printf("HARD in %v seconds...\n", delayBetweenGames)
+		time.Sleep(time.Second * delayBetweenGames)
+	} else {
+		return nil
+	}
+
+	game = NewGame(40, 17, 15, Hard)
+	result = game.Run()
+	if result.success {
+		fmt.Println("HARD completed in", result.spent.Truncate(time.Millisecond))
+		fmt.Printf("IMPOSSIBLE in %v seconds...\n", delayBetweenGames)
+		time.Sleep(time.Second * delayBetweenGames)
+	} else {
+		return nil
+	}
+
+	game = NewGame(45, 19, 30, Impossible)
+	result = game.Run()
+	if result.success {
+		fmt.Println("IMPOSSIBLE completed in", result.spent.Truncate(time.Millisecond))
+		fmt.Println("OMG YOU HAVE DONE THE WHOLE GAME! AMAIZING!")
+	}
+	return nil
 }
